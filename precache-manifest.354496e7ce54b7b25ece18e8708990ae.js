f20092852615263046bb4eaa8b4559f070edb604
Oxair <pk.ozair@gmail.com>
2019-09-01 05:09:19 +0500

    Robo Friends - React Project

---
+++ precache-manifest.354496e7ce54b7b25ece18e8708990ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28adb9c5549c634179fc302a848eac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "483194bb4b73832e571b",
+    "url": "/static/css/main.0278a68c.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "483194bb4b73832e571b",
+    "url": "/static/js/main.837e41e0.chunk.js"
+  },
+  {
+    "revision": "31175d55b7ad255ef221",
+    "url": "/static/js/runtime~main.6237261c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
